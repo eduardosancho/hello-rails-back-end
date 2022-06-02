b16b49e36f1cf9135a388cad4c79550aa1d1a29d
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-01 22:06:03 -0600

    Refactor route for greetings to use 'resources'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'greetings/index'
+      resources :greetings, only: :index
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  
+
 end

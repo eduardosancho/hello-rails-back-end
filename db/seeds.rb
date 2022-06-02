16df51d66b9fc5b65265ec463dde530dc30d8a1e
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-01 22:11:37 -0600

    Seed database with random greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.destroy_all
+
+Greeting.create(message: 'Nice to meet you')
+Greeting.create(message: 'Cheers')
+Greeting.create(message: 'Guten Tag')
+Greeting.create(message: 'Buenos dias')
+Greeting.create(message: 'Konichiwa')
+Greeting.create(message: 'Bonjour')
+Greeting.create(message: 'Buongiorno')
+Greeting.create(message: 'Servus')
+Greeting.create(message: 'Assalamu alaikum')

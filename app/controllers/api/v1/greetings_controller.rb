f419b21db1917a2ca478f542905a71c3c632b54a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-01 22:12:06 -0600

    Retrieve one random greeting on greetings#index

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    render json: { things: [
+    size = Greeting.all.count
+    greeting = Greeting.find(rand(1..size))
+    render json: { greetings: [
       {
-        name: 'some-thing',
-        guid: '4512-4587-54965'
+        message: greeting.message
       }
     ] }.to_json
   end
